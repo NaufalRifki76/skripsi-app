39628769853b3b4a6773cc8054607c727481ac7b
Faldian Pratama Wijaya <93422127+faldianpratama@users.noreply.github.com>
2023-03-15 15:18:43 +0700

    ngerapihin halaman login dan register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,60 +1,78 @@
 @extends('layout.index')
 
+<style>
+    @media screen and (max-width: 765px) {
+        .img-login {
+            display: none;
+        }
+    }
+
+    @media screen and (min-width: 766px) {
+        .img-login {
+            height: 600px;
+            border-top-left-radius: 8px;
+            border-bottom-left-radius: 8px;
+        }
+    }
+</style>
+
 @section('content')
-<section class="vh-100">
-    <div class="container-fluid">
-        
-      <div class="row">
-          <div class="col-sm-6 px-0 d-none d-sm-block">
-          <img src="https://i.pinimg.com/474x/f3/50/75/f350753b65cb5de1b2e203913b81a609.jpg"
-            alt="Login image" class="w-100 vh-100" style="object-fit: cover; object-position: left;">
+<body style="background-color: #CBEDD5">
+    <div class="mt-4 mb-4">
+        <div class="container">
+            <div class="container">
+                <div class="card shadow" style="border-radius: 8px; border: none;">
+                    <div class="row">
+                        <div class="col-md-5">
+                            <img src="https://i.pinimg.com/474x/f3/50/75/f350753b65cb5de1b2e203913b81a609.jpg"
+                                alt="Login image" class="w-100 img-login">
+                        </div>
+                        <div class="col-md-7">
+                            <div class="text-center mt-3 mb-4">
+                                <img src="{{ asset('Assets/logo/logo.png') }}" class="img-fluid me-4" width="300px"
+                                    alt="logo">
+                            </div>
+                            <div class="text-center mx-5">
+                                <form style="width: 100%;">
+
+                                    <div class="d-flex justify-content-center">
+                                        <h3 class="fw-bold px-2" style=" color: #439A97">Login</h3>
+                                    </div>
+
+                                    <div class="form-floating mb-3">
+                                        <input type="email" class="form-control" id="floatingInput"
+                                            placeholder="name@example.com">
+                                        <label for="floatingInput input-label-sm">Email</label>
+                                    </div>
+
+                                    <div class="form-group form-floating mb-3">
+                                        <input type="password" class="form-control form-control-sm"
+                                            placeholder=".form-control-sm" id="floatingInput">
+                                        <label for="floatingInput">Password</label>
+                                    </div>
+
+                                    <div>
+
+                                        <div class="d-flex justify-content-center pt-1 mb-3">
+                                            <button type="button" class="btn-green-hover">Login</button>
+                                        </div>
+
+                                        <div class="d-flex justify-content-center">
+                                            <p class="small mb-3"><a class="text-muted" href="#!">Lupa Sandi?</a></p>
+                                        </div>
+
+                                        <div class="d-flex justify-content-center mb-5">
+                                            <a href="/register" class="link-info" style=" color: #439A97">Belum Punya
+                                                Akun?</a></p>
+                                        </div>
+
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="col-sm-6 text-black">
-  
-          <div class="d-flex justify-content-center">
-            <img src="{{ asset('Assets/logo/logo.png') }}" class="img-fluid" width="300px" alt="logo">
-          </div>
-
-          <div class="d-flex align-items-center mx-3">
-  
-            <form style="width: 100%;">
-  
-              <div class="d-flex justify-content-center">
-                <h3 class="fw-bold mb-3 pb-3"  style=" color: #439A97">Login</h3>
-
-              </div>
-  
-              <div class="form-floating mb-3">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                <label for="floatingInput input-label-sm">Email</label>
-              </div>
-  
-              <div class="form-group form-floating mb-3">
-                <input type="password" class="form-control form-control-sm"  placeholder=".form-control-sm" id="floatingInput">
-                <label for="floatingInput">Password</label>
-              </div>
-  
-              <div>
-
-              <div class="d-flex justify-content-center pt-1 mb-3">
-                <button type="button" class="btn" style="background-color: #62B6B7; color: white">Login</button>
-              </div>
-
-              <div class="d-flex justify-content-center">
-                <p class="small mb-3 "><a class="text-muted" href="#!">Lupa Sandi?</a></p>
-              </div>
-
-              <div  class="d-flex justify-content-center">
-                <a href="/register" class="link-info" style=" color: #439A97">Belum Punya Akun?</a></p>
-              </div>
-  
-            </form>
-  
-          </div>
-  
-        </div>
-      
-      </div>
     </div>
-  </section>
+</body>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,78 +1,98 @@
 @extends('layout.index')
 
+<style>
+    @media screen and (max-width: 765px) {
+        .img-register {
+            display: none;
+        }
+    }
+
+    @media screen and (min-width: 766px) {
+        .img-register {
+            height: 600px;
+            border-top-left-radius: 8px;
+            border-bottom-left-radius: 8px;
+        }
+    }
+</style>
 @section('content')
-<section class="vh-100">
-    <div class="container-fluid">
-        
-      <div class="row">
-          <div class="col-sm-6 px-0 d-none d-sm-block">
-          <img src="https://i.pinimg.com/474x/f3/50/75/f350753b65cb5de1b2e203913b81a609.jpg"
-            alt="Login image" class="w-100 vh-100" style="object-fit: cover; object-position: left;">
-        </div>
-        <div class="col-sm-6 text-black">
-  
-          <div class="d-flex justify-content-center">
-            <img src="{{ asset('Assets/logo/logo.png') }}" class="img-fluid" width="300px" alt="logo">
-          </div>
-
-          <div class="d-flex align-items-center mx-3">
-  
-            <form style="width: 100%;">
-  
-              <div class="d-flex justify-content-center">
-                <h3 class="fw-bold mb-3 pb-3"  style=" color: #439A97">Register</h3>
-
-              </div>
-
-              <div class="form-floating mb-3">
-                <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
-                <label for="floatingInput">Nama Lengkap (sesuai di KTP)</label>
-              </div>
-  
-              <div class="form-floating mb-3">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                <label for="floatingInput">Email</label>
-              </div>
-
-              <div class="form-floating mb-3">
-                <input type="number" class="form-control" id="floatingInput" placeholder="name@example.com">
-                <label for="floatingInput">No. Handphone</label>
-              </div>
-
-              <div class="form-group row mt-3">
-                <div class="col-6">
-                  <div class="form-floating mb-3">
-                    <input type="password" class="form-control" id="floatingInput" placeholder="name@example.com">
-                    <label for="floatingInput">Password</label>
+<body style="background-color: #CBEDD5">
+  <div class="mt-4 mb-4">
+    <div class="container">
+      <div class="container">
+          <div class="card shadow" style="border-radius: 8px; border: none;">
+              <div class="row">
+                  <div class="col-md-5">
+                      <img src="https://i.pinimg.com/474x/f3/50/75/f350753b65cb5de1b2e203913b81a609.jpg" alt="Login image"
+                          class="w-100 img-register">
                   </div>
-                </div>
-                
-                <div class="col-6">
-                  <div class="form-floating mb-3 ">
-                    <input type="password" class="form-control" id="floatingInput" placeholder="name@example.com">
-                    <label for="floatingInput"> Ulangi Password</label>
+                  <div class="col-md-7">
+                      <div class="text-center mt-3">
+                          <img src="{{ asset('Assets/logo/logo.png') }}" class="img-fluid me-4" width="300px" alt="logo">
+                      </div>
+                      <div class="text-center mx-5">
+                          <form style="width: 100%;">
+
+                              <div class="d-flex justify-content-center">
+                                  <h3 class="fw-bold" style=" color: #439A97">Register</h3>
+
+                              </div>
+
+                              <div class="form-floating mb-3">
+                                  <input style="border-radius: 5px" type="text" class="form-control" id="floatingInput"
+                                      placeholder="name@example.com">
+                                  <label for="floatingInput">Nama Lengkap (sesuai di KTP)</label>
+                              </div>
+
+                              <div class="form-floating mb-3">
+                                  <input style="border-radius: 5px" type="email" class="form-control" id="floatingInput"
+                                      placeholder="name@example.com">
+                                  <label for="floatingInput">Email</label>
+                              </div>
+
+                              <div class="form-floating mb-3">
+                                  <input style="border-radius: 5px" type="number" class="form-control" id="floatingInput"
+                                      placeholder="name@example.com">
+                                  <label for="floatingInput">No. Handphone</label>
+                              </div>
+
+                              <div class="form-group row mt-3">
+                                  <div class="col-md-6">
+                                      <div style="border-radius: 5px" class="form-floating mb-3">
+                                          <input type="password" class="form-control" id="floatingInput"
+                                              placeholder="name@example.com">
+                                          <label for="floatingInput">Password</label>
+                                      </div>
+                                  </div>
+
+                                  <div class="col-md-6">
+                                      <div class="form-floating mb-3 ">
+                                          <input style="border-radius: 5px" type="password" class="form-control" id="floatingInput"
+                                              placeholder="name@example.com">
+                                          <label for="floatingInput">Konfirmasi Password</label>
+                                      </div>
+                                  </div>
+                              </div>
+
+
+                              <div>
+
+                                  <div class="d-flex justify-content-center pt-1 mb-2">
+                                      <button type="button" class="btn-green-hover">Register</button>
+                                  </div>
+
+                                  <div class="d-flex justify-content-center mb-3">
+                                      <a href="/login" class="link-info" style=" color: #439A97">Sudah Punya Akun?</a>
+                                      </p>
+                                  </div>
+
+                          </form>
+                      </div>
                   </div>
-                </div>
               </div>
-              
-  
-              <div>
-
-              <div class="d-flex justify-content-center pt-1 mb-3">
-                <button type="button" class="btn" style="background-color: #62B6B7; color: white">Register</button>
-              </div>
-
-              <div  class="d-flex justify-content-center mb-3">
-                <a href="/login" class="link-info" style=" color: #439A97">Sudah Punya Akun?</a></p>
-              </div>
-  
-            </form>
-  
           </div>
-  
-        </div>
-      
       </div>
     </div>
-  </section>
+  </div>
+</body>
 @endsection
