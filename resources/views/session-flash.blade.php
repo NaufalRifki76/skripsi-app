d0e10ed751be9d42dd5a7838853d44eddd1c2761
NaufalRifki76 <85022839+NaufalRifki76@users.noreply.github.com>
2023-05-25 11:15:18 +0700

    session flash

--- resources/views/session-flash.blade.php
+++ resources/views/session-flash.blade.php
@@ -14,10 +14,21 @@
     </div>
 @endif
 
-@if($errors->all())
+{{-- @if($errors->all())
     <div class="alert alert-sm alert-border-left alert-danger alert-dismissible">
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-hidden="true"></button>
         <i class="fa-solid fa-circle-info pr10"></i>
         <strong>Gagal: </strong> Silahkan login terlebih dahulu, atau register apabila anda belum mempunyai akun.
+    </div>
+@endif --}}
+
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            <strong>Gagal: Cek kembali form anda!</strong> {!! Session::get('failed') !!}
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
     </div>
 @endif
