fab4b8056292882e865d42297e5d0b1be45d3667
Faldian Pratama Wijaya <93422127+faldianpratama@users.noreply.github.com>
2023-07-06 21:15:48 +0700

    update

--- resources/views/layout/penyedia-lapangan/tolak.blade.php
+++ resources/views/layout/penyedia-lapangan/tolak.blade.php
@@ -1,5 +1,7 @@
 @extends('layout.penyedia-lapangan.layout')
-
+@php
+    use App\Models\RentHours;
+@endphp
 @section('mitra')
     <style>
         .background-img-riwayat {
@@ -25,11 +27,17 @@
                                         <div class="col-md-6 mb-3">
                                             <label for="inputState" class="form-label h5">Nama Pemesan</label>
                                             <input type="text" disabled class="form-control bg-white" id=""
-                                                placeholder="Naufal Rifki">
+                                                value="{{ $order->cust_name }}">
                                         </div>
                                         <div class="col-md-3 mb-3">
                                             <label class="h5">Status</label><br>
-                                            <h5><span class="badge bg-warning">Pending</span></h5>
+                                            @if ($order->confirmation == 0)
+                                                <h5><span class="badge bg-warning text-white">Pending</span></h5>
+                                            @elseif ($order->confirmation == 1)
+                                                <h5><span class="badge bg-success text-white">Diterima</span></h5>
+                                            @elseif ($order->confirmation == 2)
+                                                <h5><span class="badge bg-danger text-white">Ditolak</span></h5>
+                                            @endif
                                         </div>
                                         <div class="col-md-3 mb-3">
                                             <label class="h5">Bukti Transfer</label><br>
@@ -39,33 +47,200 @@
                                         <div class="col-md-6 mb-3">
                                             <label for="inputState" class="form-label h5">No Telepon Pemesan</label>
                                             <input type="text" disabled class="form-control bg-white" id=""
-                                                placeholder="02165996566">
+                                                value="{{ $order->cust_number }}">
                                         </div>
                                         <div class="col-md-6 mb-3">
                                             <label for="inputState" class="form-label h5">Email Pemesan</label>
                                             <input type="text" disabled class="form-control bg-white" id=""
-                                                placeholder="testing2gmail.com">
+                                                value="{{ $order->cust_email }}">
                                         </div>
                                         <div class="col-md-6 mb-3">
                                             <label for="inputState" class="form-label h5">Lapangan</label>
                                             <input type="text" disabled class="form-control bg-white" id=""
-                                                placeholder="A">
+                                                value="{{ $order->field }}">
                                         </div>
                                         <div class="col-md-6 mb-3">
                                             <label for="inputState" class="form-label h5">Tanggal Pemesanan Lapangan</label>
                                             <input disabled type="date" class="form-control bg-white" id=""
-                                                placeholder="">
+                                                value="{{ $order->order_date }}">
                                         </div>
+                                        @php
+                                            $hours = RentHours::where('order_id', $order->id)->first();
+                                        @endphp
                                         <div class="col-md-6">
                                             <label for="inputJam" class="form-label h5">Jam Bermain</label>
-                                            <select disabled id="inputJam" class="form-select bg-white mb-3">
-                                                <option selected value="1">09.00 - 10.00</option>
-                                            </select>
+                                            @if ($hours->up00 == 1)
+                                                <div class="col">
+                                                    <select id="inputJam" disabled class="form-select bg-white">
+                                                        <option value="">00.00 - 01.00</option>
+                                                    </select>
+                                                </div>
+                                            @endif
+                                            @if ($hours->up01 == 1)
+                                                <div class="col mb-3">
+                                                    <select id="inputJam" disabled class="form-select bg-white">
+                                                        <option value="">01.00 - 02.00</option>
+                                                    </select>
+                                                </div>
+                                            @endif
+                                            @if ($hours->up02 == 1)
+                                                <div class="col mb-3">
+                                                    <select id="inputJam" disabled class="form-select bg-white">
+                                                        <option value="">02.00 - 03.00</option>
+                                                    </select>
+                                                </div>
+                                            @endif
+                                            @if ($hours->up03 == 1)
+                                                <div class="col mb-3">
+                                                    <select id="inputJam" disabled class="form-select bg-white">
+                                                        <option value="">03.00 - 04.00</option>
+                                                    </select>
+                                                </div>
+                                            @endif
+                                            @if ($hours->up04 == 1)
+                                                <div class="col mb-3">
+                                                    <select id="inputJam" disabled class="form-select bg-white">
+                                                        <option value="">04.00 - 05.00</option>
+                                                    </select>
+                                                </div>
+                                            @endif
+                                            @if ($hours->up05 == 1)
+                                                <div class="col mb-3">
+                                                    <select id="inputJam" disabled class="form-select bg-white">
+                                                        <option value="">05.00 - 06.00</option>
+                                                    </select>
+                                                </div>
+                                            @endif
+                                            @if ($hours->up06 == 1)
+                                                <div class="col mb-3">
+                                                    <select id="inputJam" disabled class="form-select bg-white">
+                                                        <option value="">06.00 - 07.00</option>
+                                                    </select>
+                                                </div>
+                                            @endif
+                                            @if ($hours->up07 == 1)
+                                                <div class="col mb-3">
+                                                    <select id="inputJam" disabled class="form-select bg-white">
+                                                        <option value="">07.00 - 08.00</option>
+                                                    </select>
+                                                </div>
+                                            @endif
+                                            @if ($hours->up08 == 1)
+                                                <div class="col mb-3">
+                                                    <select id="inputJam" disabled class="form-select bg-white">
+                                                        <option value="">08.00 - 09.00</option>
+                                                    </select>
+                                                </div>
+                                            @endif
+                                            @if ($hours->up09 == 1)
+                                                <div class="col mb-3">
+                                                    <select id="inputJam" disabled class="form-select bg-white">
+                                                        <option value="">09.00 - 10.00</option>
+                                                    </select>
+                                                </div>
+                                            @endif
+                                            @if ($hours->up10 == 1)
+                                                <div class="col mb-3">
+                                                    <select id="inputJam" disabled class="form-select bg-white">
+                                                        <option value="">10.00 - 11.00</option>
+                                                    </select>
+                                                </div>
+                                            @endif
+                                            @if ($hours->up11 == 1)
+                                                <div class="col mb-3">
+                                                    <select id="inputJam" disabled class="form-select bg-white">
+                                                        <option value="">11.00 - 12.00</option>
+                                                    </select>
+                                                </div>
+                                            @endif
+                                            @if ($hours->up12 == 1)
+                                                <div class="col mb-3">
+                                                    <select id="inputJam" disabled class="form-select bg-white">
+                                                        <option value="">12.00 - 13.00</option>
+                                                    </select>
+                                                </div>
+                                            @endif
+                                            @if ($hours->up13 == 1)
+                                                <div class="col mb-3">
+                                                    <select id="inputJam" disabled class="form-select bg-white">
+                                                        <option value="">13.00 - 14.00</option>
+                                                    </select>
+                                                </div>
+                                            @endif
+                                            @if ($hours->up14 == 1)
+                                                <div class="col mb-3">
+                                                    <select id="inputJam" disabled class="form-select bg-white">
+                                                        <option value="">14.00 - 15.00</option>
+                                                    </select>
+                                                </div>
+                                            @endif
+                                            @if ($hours->up15 == 1)
+                                                <div class="col mb-3">
+                                                    <select id="inputJam" disabled class="form-select bg-white">
+                                                        <option value="">15.00 - 16.00</option>
+                                                    </select>
+                                                </div>
+                                            @endif
+                                            @if ($hours->up16 == 1)
+                                                <div class="col mb-3">
+                                                    <select id="inputJam" disabled class="form-select bg-white">
+                                                        <option value="">16.00 - 17.00</option>
+                                                    </select>
+                                                </div>
+                                            @endif
+                                            @if ($hours->up17 == 1)
+                                                <div class="col mb-3">
+                                                    <select id="inputJam" disabled class="form-select bg-white">
+                                                        <option value="">17.00 - 18.00</option>
+                                                    </select>
+                                                </div>
+                                            @endif
+                                            @if ($hours->up18 == 1)
+                                                <div class="col mb-3">
+                                                    <select id="inputJam" disabled class="form-select bg-white">
+                                                        <option value="">18.00 - 19.00</option>
+                                                    </select>
+                                                </div>
+                                            @endif
+                                            @if ($hours->up19 == 1)
+                                                <div class="col mb-3">
+                                                    <select id="inputJam" disabled class="form-select bg-white">
+                                                        <option value="">19.00 - 20.00</option>
+                                                    </select>
+                                                </div>
+                                            @endif
+                                            @if ($hours->up20 == 1)
+                                                <div class="col mb-3">
+                                                    <select id="inputJam" disabled class="form-select bg-white">
+                                                        <option value="">20.00 - 21.00</option>
+                                                    </select>
+                                                </div>
+                                            @endif
+                                            @if ($hours->up21 == 1)
+                                                <div class="col mb-3">
+                                                    <select id="inputJam" disabled class="form-select bg-white">
+                                                        <option value="">21.00 - 22.00</option>
+                                                    </select>
+                                                </div>
+                                            @endif
+                                            @if ($hours->up22 == 1)
+                                                <div class="col mb-3">
+                                                    <select id="inputJam" disabled class="form-select bg-white">
+                                                        <option value="">22.00 - 23.00</option>
+                                                    </select>
+                                                </div>
+                                            @endif
+                                            @if ($hours->up23 == 1)
+                                                <div class="col mb-3">
+                                                    <select id="inputJam" disabled class="form-select bg-white">
+                                                        <option value="">23.00 - 00.00</option>
+                                                    </select>
+                                                </div>
+                                            @endif
                                         </div>
                                         <div class="col-md-6 mb-3">
                                             <label for="inputState" class="form-label h5">Total Biaya</label>
-                                            <input type="text" disabled class="form-control bg-white" id=""
-                                                placeholder="Rp. 155000">
+                                            <input type="text" disabled class="form-control bg-white" id="" value="Rp. {{ $order->price_sum }}">
                                         </div>
                                     </div>
                                 </div>
