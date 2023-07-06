04dc0311a0650f2e2ce909e93af5233136d570b3
Faldian Pratama Wijaya <93422127+faldianpratama@users.noreply.github.com>
2023-07-06 17:24:05 +0700

    update

--- resources/views/riwayat-pemesanan/index.blade.php
+++ resources/views/riwayat-pemesanan/index.blade.php
@@ -4,113 +4,121 @@
 @endphp
 @section('content')
 
-<style>
-    .background-img-riwayat {
-    background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url("Assets/riwayat-pemesanan/bg.jpg");
-    }
-</style>
+    <style>
+        .background-img-riwayat {
+            background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url("Assets/riwayat-pemesanan/bg.jpg");
+        }
+    </style>
 
-<div style="background-color: #ECFAF0">
-    <div class="row">
-        <div class="col-2"></div>
-        <div class="col-8 text-center py-4"
-            style="background-color: #439a97; border-bottom-right-radius: 12px; border-bottom-left-radius: 12px">
-            <h1 style="color: white">Riwayat Pemesanan Anda</h1>
-            <h5 style="color: #FCE700">Cek status pemesanan lapangan yang telah anda lakukan di sini!</h5>
+    <div style="background-color: #ECFAF0">
+        <div class="row">
+            <div class="col-2"></div>
+            <div class="col-8 text-center py-4"
+                style="background-color: #439a97; border-bottom-right-radius: 12px; border-bottom-left-radius: 12px">
+                <h1 style="color: white">Riwayat Pemesanan Anda</h1>
+                <h5 style="color: #FCE700">Cek status pemesanan lapangan yang telah anda lakukan di sini!</h5>
+            </div>
+            <div class="col-2"></div>
         </div>
         <div class="col-2"></div>
-    </div>
-    <div class="row">
-        <div class="col-1"></div>
-        <div class="col-10">
-            <form action="{{ route('riwayat-order') }}" method="post">
-                {{ csrf_field() }}
-                <div class="row py-4 mx-5">
-                    <div class="col-md-10 mb-2">
-                        <div class="form-floating">
-                            <input type="text" class="form-control" id="nama-lapangan"
+        <div class="row">
+            <div class="col-1"></div>
+            <div class="col-10">
+                <form action="{{ route('riwayat-order') }}" method="post">
+                    {{ csrf_field() }}
+                    <div class="row py-4 mx-5">
+                        <div class="col-md-10 mb-2">
+                            <div class="form-floating">
+                                <input type="text" class="form-control" id="nama-lapangan"
                                 placeholder="Nama Lapangan Yang Anda Cari ..." name="search">
-                            <label for="nama-lapangan">Nama Lapangan</label>
+                                <label for="nama-lapangan">Nama Lapangan</label>
+                            </div>
+                        </div>
+                        <div class="col-md-2 text-center">
+                            <button type="submit" id="" name="" class="btn-green-transition mt-2">Cari Lapangan</button>
                         </div>
                     </div>
-                    <div class="col-md-2 text-center">
-                        <button type="submit" id="" name="" class="btn-green-transition mt-2">Cari Lapangan</button>
-                    </div>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
-        <div class="col-1"></div>
     </div>
-</div>
-<div class="container">
-    <div class="row">
-        <div class="col-md-2"></div>
-        <div class="col-md-8">
-            @if ($orders == null)
-                <h5 class="my-5" style="color: #439a97">Anda belum pernah melakukan order!</h5>
-            @else
-                <h5 class="my-5" style="color: #439a97">Harap perlihatkan bukti pemesanan pada pihak penyedia lapangan yang anda pesan!</h5>
-                @foreach ($orders as $key => $order)
-                    <div class="card shadow-lg background-img-riwayat mb-5" style="border: none; border-radius: 12px">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-md-7">
-                                    <div class="mb-3">
-                                        <label for="inputState" class="form-label text-white h5">Nama Pemesan</label>
-                                        <input type="text" disabled class="form-control bg-white" id="" value="{{$order->cust_name}}"
-                                            placeholder="">
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="inputState" class="form-label text-white h5">Nama Lapangan</label>
-                                        <input type="text" disabled class="form-control bg-white" id="" value="{{$order->venue_name}}"
-                                            placeholder="">
-                                    </div>
-                                </div>
-                                <div class="col-md-5" style="margin-top: 35px">
-                                    <div class="card bg-transparent border-white border-3" style="border-radius: 12px;">
-                                        <div class="card-body text-center">
-                                            <h5 class="mb-2">
-                                            <b class="text-white">Status Pemesanan:</b> 
-                                            @if ($order->confirmation == 0)
-                                                <span class="badge bg-warning shadow">Menunggu</span>
-                                            @elseif ($order->confirmation == 1)
-                                                <span class="badge bg-success shadow">Berhasil</span>
-                                            @elseif ($order->confirmation == 2)
-                                                <span class="badge bg-danger shadow">Dibatalkan</span>
-                                            @endif
-                                            </h5>
-                                            @if ($order->confirmation == 0)
-                                                <p class="fs-6 text-white" style="margin-bottom: -5px">  Menunggu di setujui oleh pihak lapangan!</p>
-                                            @elseif ($order->confirmation == 1)
-                                                <p class="fs-6" style="margin-bottom: -5px">  Pesanan anda berhasil!</p>
-                                            @elseif ($order->confirmation == 2)
-                                                <p class="fs-6" style="margin-bottom: -5px">  Pesanan anda dibatalkan!
-                                            @endif
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2"></div>
+            <div class="col-md-8">
+                @if ($orders == null)
+                    <h5 class="my-5" style="color: #439a97">Anda belum pernah melakukan order!</h5>
+                @else
+                    <h5 class="my-5" style="color: #439a97">Harap perlihatkan bukti pemesanan pada pihak penyedia lapangan
+                        yang anda pesan!</h5>
+                    @foreach ($orders as $key => $order)
+                        <div class="card shadow-lg background-img-riwayat mb-5" style="border: none; border-radius: 12px">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-7">
+                                        <div class="mb-3">
+                                            <label for="inputState" class="form-label text-white h5">Nama Pemesan</label>
+                                            <input type="text" disabled class="form-control bg-white" id=""
+                                                value="{{ $order->cust_name }}" placeholder="">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="inputState" class="form-label text-white h5">Nama Lapangan</label>
+                                            <input type="text" disabled class="form-control bg-white" id=""
+                                                value="{{ $order->venue_name }}" placeholder="">
                                         </div>
                                     </div>
-                                </div>
-                                <div class="col-md-6 mb-3">
-                                    <label for="inputState" class="form-label text-white h5">No Telpon Pemesan</label>
-                                    <input type="text" disabled class="form-control bg-white" id="" value="{{$order->cust_number}}" placeholder="">
-                                </div>
-                                <div class="col-md-6 mb-3">
-                                    <label for="inputState" class="form-label text-white h5">Email Pemesan</label>
-                                    <input type="text" disabled class="form-control bg-white" id="" value="{{$order->cust_email}}" placeholder="">
-                                </div>
-                                <div class="col-md-6 mb-3">
-                                    <label for="inputState" class="form-label text-white h5">Lapangan</label>
-                                    <input type="text" disabled class="form-control bg-white" id="" value="{{$order->field}}" placeholder="">
-                                </div>
-                                <div class="col-md-6 mb-3">
-                                    <label for="inputState" class="form-label text-white h5">Tanggal Pemesanan Lapangan</label>
-                                    <input disabled type="date" class="form-control bg-white" id="" value="{{$order->order_date}}" placeholder="">
-                                </div>
-                                @php
-                                    $hours = RentHours::where('order_id', $order->id)->first();
-                                @endphp
-                                <label for="inputJam" class="form-label text-white h5">Jam Bermain</label>
-                                <div class="row">
+                                    <div class="col-md-5" style="margin-top: 35px">
+                                        <div class="card bg-transparent border-white border-3" style="border-radius: 12px;">
+                                            <div class="card-body text-center">
+                                                <h5 class="mb-2">
+                                                    <b class="text-white">Status Pemesanan:</b>
+                                                    @if ($order->confirmation == 0)
+                                                        <span class="badge bg-warning shadow">Menunggu</span>
+                                                    @elseif ($order->confirmation == 1)
+                                                        <span class="badge bg-success shadow">Berhasil</span>
+                                                    @elseif ($order->confirmation == 2)
+                                                        <span class="badge bg-danger shadow">Dibatalkan</span>
+                                                    @endif
+                                                </h5>
+                                                @if ($order->confirmation == 0)
+                                                    <p class="fs-6 text-white" style="margin-bottom: -5px"> Menunggu di
+                                                        setujui oleh pihak lapangan!</p>
+                                                @elseif ($order->confirmation == 1)
+                                                    <p class="fs-6" style="margin-bottom: -5px"> Pesanan anda berhasil!
+                                                    </p>
+                                                @elseif ($order->confirmation == 2)
+                                                    <p class="fs-6" style="margin-bottom: -5px"> Pesanan anda dibatalkan!
+                                                @endif
+                                            </div>
+                                        </div>
+                                    </div>
                                     <div class="col-md-6 mb-3">
+                                        <label for="inputState" class="form-label text-white h5">No Telpon Pemesan</label>
+                                        <input type="text" disabled class="form-control bg-white" id=""
+                                            value="{{ $order->cust_number }}" placeholder="">
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <label for="inputState" class="form-label text-white h5">Email Pemesan</label>
+                                        <input type="text" disabled class="form-control bg-white" id=""
+                                            value="{{ $order->cust_email }}" placeholder="">
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <label for="inputState" class="form-label text-white h5">Lapangan</label>
+                                        <input type="text" disabled class="form-control bg-white" id=""
+                                            value="{{ $order->field }}" placeholder="">
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <label for="inputState" class="form-label text-white h5">Tanggal Pemesanan
+                                            Lapangan</label>
+                                        <input disabled type="date" class="form-control bg-white" id=""
+                                            value="{{ $order->order_date }}" placeholder="">
+                                    </div>
+                                    @php
+                                        $hours = RentHours::where('order_id', $order->id)->first();
+                                    @endphp
+
+                                    <div class="col-md-6 mb-3">
+                                        <label for="inputJam" class="form-label text-white h5">Jam Bermain</label>
                                         @if ($hours->up00 == 1)
                                             <div class="col">
                                                 <select id="inputJam" disabled class="form-select bg-white">
@@ -280,29 +288,26 @@
                                             </div>
                                         @endif
                                     </div>
-                                    <div class="col-md-6">
-                                        <div class="col mb-3">
-                                            <label for="inputState" class="form-label text-white h5">Total Biaya</label>
-                                            <input type="text" disabled class="form-control bg-white" id="" value="Rp. {{$order->price_sum}}"
-                                                placeholder="">
+                                    <div class="col-md-6 mb-3">
+                                        <label for="inputState" class="form-label text-white h5">Total Biaya</label>
+                                        <input type="text" disabled class="form-control bg-white" id=""
+                                            value="Rp. {{ $order->price_sum }}" placeholder="">
+                                    </div>
+                                    @if ($order->confirmation == 2)
+                                        <div class="form-group">
+                                            <label for="cancel_reason" class="h5">Alasan Pemesanan Ditolak</label>
+                                            <textarea class="form-control border-2" style="background-color: #fff" id="cancel_reason" name="cancel_reason"
+                                                rows="3" placeholder="">{{ $order->cancel_reason }}</textarea>
                                         </div>
-                                    </div>
+                                    @endif
                                 </div>
-                                @if ($order->confirmation == 2)
-                                    <div class="form-group">
-                                        <label for="cancel_reason" class="h5">Alasan Pemesanan Ditolak</label>
-                                        <textarea class="form-control border-2" style="background-color: #fff" id="cancel_reason" name="cancel_reason" rows="3"
-                                            placeholder="">{{$order->cancel_reason}}</textarea>
-                                    </div>
-                                @endif
                             </div>
                         </div>
-                    </div>
-                @endforeach
-            @endif
+                    @endforeach
+                @endif
+            </div>
+            <div class="col-md-2"></div>
         </div>
-        <div class="col-md-2"></div>
     </div>
-</div>
 
 @endsection
