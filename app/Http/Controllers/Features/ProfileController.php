6d07e4739d2c8c978344d69d9810c34dbfb4804a
Faris Hakim <100683009+farishakim16@users.noreply.github.com>
2023-07-06 11:18:22 +0700

    profile

--- app/Http/Controllers/Features/ProfileController.php
+++ app/Http/Controllers/Features/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Features;
 
 use App\Http\Controllers\Controller;
 use App\Models\RentOrder;
+use App\Models\User;
 use Cartalyst\Sentinel\Laravel\Facades\Sentinel;
 use Illuminate\Http\Request;
 
@@ -15,6 +16,17 @@ class ProfileController extends Controller{
             $user = Sentinel::getUser();
             $orders = RentOrder::where('user_id', $user->id)->get();
             return view('riwayat-pemesanan.index', compact('orders'));
+        }
+    }
+    public function profile(){
+        if(!Sentinel::getUser()) {
+                return redirect()->route('return.login')->with('failed', 'Silahkan login terlebih dahulu!');
+        } else {
+            $user = Sentinel::getUser();
+          //  $users = User::where('user_id', $user->id)->get();
+            
+
+            return view('profile.index');
         }
     }
 }
